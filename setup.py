83f799265dc61d5854155b6b49c5a88d8b99c121
Jeffrey Nappi <jeff@iacquire.com>
2014-01-29 08:23:26 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2007-2008 Nathanael C. Fritz
-# All Rights Reserved
-#
-# This software is licensed as described in the README file,
-# which you should have received as part of this distribution.
-#
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-VERSION = 'v14.01.27a'
+VERSION = 'v14.01.29a'
 DESCRIPTION = 'sfjson'
 LONG_DESCRIPTION = """
 sfjson is a Superfeedr JSON wrapper for SleekXMPP
 SleekXMPP is an elegant Python library for XMPP (aka Jabber, Google Talk, etc).
 """
-
-CLASSIFIERS = ['Intended Audience :: Developers',
-               'License :: OSI Approved :: MIT License',
-               'Programming Language :: Python',
-               'Topic :: Software Development :: Libraries :: Python Modules',
-]
 
 setup(
     name="sfjson",
@@ -32,8 +21,12 @@ setup(
     author_email='jeff [at] iacquire.com',
     url='https://github.com/iAcquire/sfjson-python',
     license='MIT',
-    platforms=['any'],
     packages=['sfjson'],
-    requires=['sleekxmpp', 'tlslite', 'pyasn1', 'pyasn1-modules', 'python-dateutil']
+    install_requires=['sleekxmpp', 'tlslite', 'pyasn1', 'pyasn1-modules', 'python-dateutil'],
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    )
 )
-
